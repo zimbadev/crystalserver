0eb5208e03c2be9b739c2a57a20507a94b2fa777
João Paulo <jprzimba@gmail.com>
2024-11-28 14:13:10 -0300

    * Move migrations to core

--- src/database/databasemanager.cpp
+++ src/database/databasemanager.cpp
@@ -93,7 +93,7 @@ void DatabaseManager::updateDatabase() {
 	int32_t version = getDatabaseVersion();
 	do {
 		std::ostringstream ss;
-		ss << g_configManager().getString(DATA_DIRECTORY) + "/migrations/" << version << ".lua";
+		ss << g_configManager().getString(CORE_DIRECTORY) + "/migrations/" << version << ".lua";
 		if (luaL_dofile(L, ss.str().c_str()) != 0) {
 			g_logger().error("DatabaseManager::updateDatabase - Version: {}"
 			                 "] {}",
