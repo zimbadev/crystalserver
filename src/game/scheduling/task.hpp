5321f9b523b7560fce6825f559e2b663b398979f
João Paulo <jprzimba@gmail.com>
2024-12-07 11:26:55 -0300

    * Fix check in creatureCheck
    Thanks to @mehah

--- src/game/scheduling/task.hpp
+++ src/game/scheduling/task.hpp
@@ -83,6 +83,7 @@ private:
 			"Game::createInfluencedMonsters",
 			"Game::updateCreatureWalk",
 			"Game::updateForgeableMonsters",
+			"Game::addCreatureCheck",
 			"GlobalEvents::think",
 			"LuaEnvironment::executeTimerEvent",
 			"Modules::executeOnRecvbyte",
