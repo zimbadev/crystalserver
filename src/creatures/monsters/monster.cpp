68f1ce91f7b24ded91df851eed81c52fcdc621cf
João Paulo <jprzimba@gmail.com>
2025-01-12 08:49:44 -0300

    * Fix removeTarget crash

--- src/creatures/monsters/monster.cpp
+++ src/creatures/monsters/monster.cpp
@@ -612,7 +612,11 @@ bool Monster::removeTarget(const std::shared_ptr<Creature> &creature) {
 		totalPlayersOnScreen--;
 	}
 
-	targetList.erase(it);
+	if (auto shared = it->lock()) {
+		targetList.erase(it);
+	} else {
+		return false;
+	}
 
 	return true;
 }
