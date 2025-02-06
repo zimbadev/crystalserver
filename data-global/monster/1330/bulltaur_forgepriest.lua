9709f5f1b0bfa6f9d016145c37ce2883ed52ba93
João Paulo <jprzimba@gmail.com>
2025-02-06 09:42:40 -0300

    * Add missing monsters, spells and updated items.xml
    * Fix corpses

--- data-global/monster/1330/bulltaur_forgepriest.lua
+++ data-global/monster/1330/bulltaur_forgepriest.lua
@@ -1,8 +1,8 @@
-local mType = Game.createMonsterType("Bulltaur Alchemist")
+local mType = Game.createMonsterType("Bulltaur Forgepriest")
 local monster = {}
 
-monster.description = "a bulltaur alchemist"
-monster.experience = 4350
+monster.description = "a bulltaur forgepriest"
+monster.experience = 5180
 monster.outfit = {
 	lookType = 1718,
 	lookHead = 0,
@@ -13,7 +13,7 @@ monster.outfit = {
 	lookMount = 0,
 }
 
-monster.raceId = 2448
+monster.raceId = 2449
 monster.Bestiary = {
 	class = "Humanoid",
 	race = BESTY_RACE_HUMANOID,
@@ -26,10 +26,10 @@ monster.Bestiary = {
 	Locations = "Demonwar Crypt.",
 }
 
-monster.health = 5690
-monster.maxHealth = 5690
+monster.health = 6840
+monster.maxHealth = 6840
 monster.race = "blood"
-monster.corpse = 44712
+monster.corpse = 44716
 monster.speed = 165
 monster.manaCost = 0
 
@@ -56,7 +56,7 @@ monster.flags = {
 	canPushItems = true,
 	canPushCreatures = true,
 	staticAttackChance = 80,
-	targetDistance = 1,
+	targetDistance = 2,
 	runHealth = 0,
 	healthHidden = false,
 	isBlockable = false,
@@ -84,7 +84,7 @@ monster.loot = {
 	{ id = 44736, chance = 4545 }, -- stealth ring
 	{ id = 44737, chance = 4545 }, -- stealth ring
 	{ id = 44738, chance = 4545 }, -- stealth ring
-	{ name = "might ring", chance = 4285 },
+	{ name = "might ring", chance = 14285 },
 	{ name = "mino shield", chance = 840 },
 	{ name = "dragon hammer", chance = 420 },
 	{ id = 3041, chance = 4545 }, -- stealth ring
@@ -94,35 +94,43 @@ monster.loot = {
 	{ name = "encrypted notes", chance = 985 },
 	{ name = "ultimate mana potion", chance = 1285 },
 	{ name = "gold ingot", chance = 1285 },
+	{ name = "lightning robe", chance = 500, maxCount = 1 },
+	{ name = "small topaz", chance = 1200, maxCount = 3 },
+	{ name = "stone skin amulet", chance = 1200, maxCount = 1 },
+	{ name = "spellbook of mind control", chance = 1200, maxCount = 1 },
+	{ name = "wand of defiance", chance = 1200, maxCount = 1 },
 }
 
 monster.attacks = {
 	{ name = "melee", interval = 2000, chance = 100, minDamage = 90, maxDamage = -390 },
 	{ name = "combat", interval = 2000, chance = 20, type = COMBAT_ICEDAMAGE, minDamage = -150, maxDamage = -450, range = 3, radius = 4, effect = CONST_ME_ICEAREA, target = true },
 	{ name = "combat", interval = 2000, chance = 20, type = COMBAT_ENERGYDAMAGE, minDamage = -150, maxDamage = -450, range = 3, radius = 4, effect = 170, target = true },
+	{ name = "speed", interval = 2000, chance = 15, speedChange = -650, range = 7, radius = 4, effect = 168, target = false, duration = 1500 },
+	{ name = "energy wave", interval = 2000, chance = 15, minDamage = -350, maxDamage = -500, length = 1, spread = 0, effect = CONST_ME_ENERGYAREA, target = true },
+	{ name = "combat", interval = 2000, chance = 20, type = COMBAT_ENERGYDAMAGE, minDamage = -150, maxDamage = -450, range = 3, radius = 4, effect = 170, target = true },
 }
 
 monster.defenses = {
 	defense = 30,
-	armor = 78,
-	mitigation = 1.22,
+	armor = 73,
+	mitigation = 2.22,
 }
 
 monster.elements = {
-	{ type = COMBAT_PHYSICALDAMAGE, percent = 30 },
+	{ type = COMBAT_PHYSICALDAMAGE, percent = 20 },
 	{ type = COMBAT_ENERGYDAMAGE, percent = 0 },
-	{ type = COMBAT_EARTHDAMAGE, percent = -20 },
-	{ type = COMBAT_FIREDAMAGE, percent = 0 },
+	{ type = COMBAT_EARTHDAMAGE, percent = 0 },
+	{ type = COMBAT_FIREDAMAGE, percent = 15 },
 	{ type = COMBAT_LIFEDRAIN, percent = 0 },
 	{ type = COMBAT_MANADRAIN, percent = 0 },
 	{ type = COMBAT_DROWNDAMAGE, percent = 0 },
-	{ type = COMBAT_ICEDAMAGE, percent = 15 },
-	{ type = COMBAT_HOLYDAMAGE, percent = -5 },
-	{ type = COMBAT_DEATHDAMAGE, percent = -5 },
+	{ type = COMBAT_ICEDAMAGE, percent = -10 },
+	{ type = COMBAT_HOLYDAMAGE, percent = 0 },
+	{ type = COMBAT_DEATHDAMAGE, percent = -10 },
 }
 
 monster.immunities = {
-	{ type = "paralyze", condition = false },
+	{ type = "paralyze", condition = true },
 	{ type = "outfit", condition = false },
 	{ type = "invisible", condition = true },
 	{ type = "bleed", condition = false },
