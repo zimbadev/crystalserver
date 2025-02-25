bed4882ea5f75b0d0aac5d76513c91d108f198a6
João Paulo <jprzimba@gmail.com>
2025-02-25 08:04:43 -0300

    [fix] /pos command
    This PR fixes /pos command show to all

--- data/scripts/talkactions/gm/position.lua
+++ data/scripts/talkactions/gm/position.lua
@@ -7,25 +7,28 @@ function position.onSay(player, words, param)
 	if param == "" then
 		local pos = player:getPosition()
 		player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "Your current position is: " .. pos.x .. ", " .. pos.y .. ", " .. pos.z .. ".")
-		return
+		return true
 	end
 
 	local teleportPosition = param:toPosition()
 	if not teleportPosition then
 		player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "Invalid position format. Use one of the following formats: \n/pos {x = ..., y = ..., z = ...}\n/pos Position(..., ..., ...)\n/pos x, y, z.")
-		return
+		return true
 	end
 
 	local tile = Tile(teleportPosition)
 	if not tile then
 		player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "Invalid tile or position. Send a valid position.")
-		return
+		return true
 	end
 
 	player:teleportTo(teleportPosition)
 	if not player:isInGhostMode() then
 		teleportPosition:sendMagicEffect(CONST_ME_TELEPORT)
+		return true
 	end
+
+	return true
 end
 
 position:separator(" ")
