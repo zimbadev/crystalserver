24302500fdae90f1770f13b86095df83e674f53d
Paco <frjafopa@gmail.com>
2025-05-05 02:17:10 -0600

    Update KickHouse Spell
    Check if player is properly standing on a house tile to avoid error in console.

--- data/scripts/spells/house/kick.lua
+++ data/scripts/spells/house/kick.lua
@@ -1,30 +1,41 @@
 local spell = Spell("instant")
 
 function spell.onCastSpell(player, variant)
-	local targetPlayer = Player(variant:getString()) or player
-	local guest = targetPlayer:getTile():getHouse()
-	local owner = player:getTile():getHouse()
+    local targetPlayer = Player(variant:getString()) or player
+    local targetTile = targetPlayer:getTile()
+    if not targetTile then
+        player:sendCancelMessage(RETURNVALUE_NOTPOSSIBLE)
+        player:getPosition():sendMagicEffect(CONST_ME_POFF)
+        return false
+    end
 
-	-- Owner kick yourself from house
-	if targetPlayer == player then
-		player:getPosition():sendMagicEffect(CONST_ME_POFF)
-		player:teleportTo(owner:getExitPosition())
-		player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
-		return true
-	end
+    local guestHouse = targetTile:getHouse()
+    local ownerHouse = player:getTile():getHouse()
 
-	if not owner:canEditAccessList(GUEST_LIST, player) then
-		player:sendCancelMessage(RETURNVALUE_NOTPOSSIBLE)
-		player:getPosition():sendMagicEffect(CONST_ME_POFF)
-		return false
-	end
+    if targetPlayer == player then
+        if not ownerHouse then
+            player:sendCancelMessage(RETURNVALUE_NOTPOSSIBLE)
+            player:getPosition():sendMagicEffect(CONST_ME_POFF)
+            return false
+        end
+        player:getPosition():sendMagicEffect(CONST_ME_POFF)
+        player:teleportTo(ownerHouse:getExitPosition())
+        player:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
+        return true
+    end
 
-	if not owner or not guest or not guest:kickPlayer(player, targetPlayer) then
-		player:sendCancelMessage(RETURNVALUE_NOTPOSSIBLE)
-		player:getPosition():sendMagicEffect(CONST_ME_POFF)
-		return false
-	end
-	return true
+    if not ownerHouse or not ownerHouse:canEditAccessList(GUEST_LIST, player) then
+        player:sendCancelMessage(RETURNVALUE_NOTPOSSIBLE)
+        player:getPosition():sendMagicEffect(CONST_ME_POFF)
+        return false
+    end
+
+    if not guestHouse or not guestHouse:kickPlayer(player, targetPlayer) then
+        player:sendCancelMessage(RETURNVALUE_NOTPOSSIBLE)
+        player:getPosition():sendMagicEffect(CONST_ME_POFF)
+        return false
+    end
+    return true
 end
 
 spell:name("House Kick")
