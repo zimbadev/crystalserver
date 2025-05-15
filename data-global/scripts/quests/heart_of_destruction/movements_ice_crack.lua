043864ef749fd5633c3886de3e5785db4b5ba823
Paco <frjafopa@gmail.com>
2025-05-15 05:51:06 -0600

    heartofdestruction-tile-fix (#155)
    The ice hole was creating over and over again in the same spot every time. The script now check if there's a hole there and auto deletes itself after 10 seconds.

--- data-global/scripts/quests/heart_of_destruction/movements_ice_crack.lua
+++ data-global/scripts/quests/heart_of_destruction/movements_ice_crack.lua
@@ -32,6 +32,16 @@ function iceCrack.onStepIn(creature, item, position, fromPosition)
 	if not player then
 		return true
 	end
+
+	local holePos = { x = 32164, y = 31293, z = 6 }
+	local tile = Tile(holePos)
+	if not tile or not tile:getItemById(610) then
+		local holeItem = Game.createItem(610, 1, holePos)
+		if holeItem then
+			holeItem:decay()
+		end
+	end
+
 	if Game.getStorageValue(GlobalStorage.IceCrack) ~= 1 then
 		for a = 1, #config do
 			Game.createItem(config[a].itemId, 1, config[a].position)
