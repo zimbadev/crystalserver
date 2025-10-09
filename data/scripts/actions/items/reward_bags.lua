a03ef04a02d49db1a8a6d86b26c6719414bef0eb
Mateusz Maciejewski <mmateusz@grefekt.pl>
2025-10-09 23:47:05 +0200

    feat: dodanie KosOTS Bag'a

--- data/scripts/actions/items/reward_bags.lua
+++ data/scripts/actions/items/reward_bags.lua
@@ -51,6 +51,28 @@ local rewardBags = {
 		{ id = 43885, name = "sanguine rod" },
 		{ id = 43887, name = "sanguine galoshes" },
 	},
+ 	[KOSOTS_BAG] = {
+ 		{ id = 36723, name = "kooldown-aid" },
+ 		{ id = 36724, name = "strike enhancement" },
+ 		{ id = 36725, name = "stamina extension" },
+ 		{ id = 36726, name = "charm upgrade" },
+ 		{ id = 36727, name = "wealth duplex" },
+ 		{ id = 36728, name = "bestiary betterment" },
+ 		{ id = 36729, name = "fire resilience" },
+ 		{ id = 36730, name = "ice resilience" },
+ 		{ id = 36731, name = "earth resilience" },
+ 		{ id = 36732, name = "energy resilience" },
+ 		{ id = 36733, name = "holy resilience" },
+ 		{ id = 36734, name = "death resilience" },
+ 		{ id = 36735, name = "physical resilience" },
+ 		{ id = 36736, name = "fire amplification" },
+ 		{ id = 36737, name = "ice amplification" },
+ 		{ id = 36738, name = "earth amplification" },
+ 		{ id = 36739, name = "energy amplification" },
+ 		{ id = 36740, name = "holy amplification" },
+ 		{ id = 36741, name = "death amplification" },
+ 		{ id = 36742, name = "physical amplification" },
+ 	},
 }
 
 local randomItems = Action()
