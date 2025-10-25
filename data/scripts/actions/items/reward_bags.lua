6a19e3013c72fc1c317411c03e35e84df3c299b6
Sh3Ns <50599143+Sh3Ns@users.noreply.github.com>
2025-10-25 05:38:31 -0300

    Add ethereal coned hat to reward bags
    Added new reward item "ethereal coned hat" (ID: 50188) to the reward bag loot table. This item is included alongside other arboreal-themed rewards like arboreal crown, tome, and charged ring.

--- data/scripts/actions/items/reward_bags.lua
+++ data/scripts/actions/items/reward_bags.lua
@@ -33,6 +33,7 @@ local rewardBags = {
 		{ id = 39153, name = "arboreal crown" },
 		{ id = 39154, name = "arboreal tome" },
 		{ id = 39186, name = "charged arboreal ring" },
+		{ id = 50188, name = "ethereal coned hat" },
 	},
 
 	[BAG_YOU_COVET] = {
