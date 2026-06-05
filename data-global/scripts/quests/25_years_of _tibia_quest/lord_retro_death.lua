c838ebf72a4d1d72c901532b52334c0af3a9bf8b
João Paulo <jprzimba@gmail.com>
2026-06-05 11:34:48 -0300

    fix: Add 25-years quest scripts, refactor time machine
    NOTE: need to do storages parte, this is only the basics

--- data-global/scripts/quests/25_years_of _tibia_quest/lord_retro_death.lua
+++ data-global/scripts/quests/25_years_of _tibia_quest/lord_retro_death.lua
@@ -2,15 +2,10 @@ local lordRetroDeath = CreatureEvent("LordRetroMonsterDeath")
 
 function lordRetroDeath.onDeath(creature)
 	onDeathForDamagingPlayers(creature, function(_, player)
-		local hasMaleFormal = player:hasOutfit(1460)
-		local hasFemaleFormal = player:hasOutfit(1461)
+		local hasOutfit = player:hasOutfit(1460) and player:hasOutfit(1461)
 
-		if not hasMaleFormal then
+		if not hasOutfit then
 			player:addOutfit(1460)
-			player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You have unlocked the Formal Dress outfit.")
-		end
-
-		if not hasFemaleFormal then
 			player:addOutfit(1461)
 			player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You have unlocked the Formal Dress outfit.")
 		end
